3cc656da55669b77881285a3e6b21b6383fd79e6
Yrka <varnack30@gmail.com>
2024-01-12 20:43:15 +0300

    Сделал базовую разметку без картинок и данных

--- app/src/main/java/com/example/flowers/MainActivity.java
+++ app/src/main/java/com/example/flowers/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.flowers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
